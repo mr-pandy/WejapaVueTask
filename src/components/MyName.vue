3c46e899c06de68f0826fa6b17c8bec1819a216b
mr-pandy <ekerekepreciousimeh@gmail.com>
2020-07-30 02:35:50 +0100

    Wejapa

---
+++ src/components/MyName.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Wejapa Task</h1>
+        <p> {{ name }} </p>
+        <p> {{ slack }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  props: {
+    name : String,
+    slack: String
+  }
+}
+</script>
